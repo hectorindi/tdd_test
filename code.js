c220c71c1e25bd02832cea92ddadf663070af182
DESKTOP-50GTALS\Piyush <piyushnegi1989@gmail.com>
2022-04-30 16:32:30 +0530

    passing all test case

--- code.js
+++ code.js
@@ -1,10 +1,15 @@
+let constants = {
+  delim: ",",
+  str: "",
+};
 /**
  * Add 2 number spaprated my a comma , input as a string
  */
 function add(numString) {
   let result;
-  if (!numString) return 0;
-  let allNums = getAllValues(numString);
+  constants.str = numString;
+  if (!constants.str) return 0;
+  let allNums = getAllValues();
   if (allNums.length < 2) return 0;
   result = allNums.reduce(
     (prev, curr) => parseInt(prev.toString()) + parseInt(curr.toString())
@@ -17,15 +22,18 @@ function add(numString) {
  */
 
 function getAllValues(str) {
-  let matched = str.match(",");
+  constants.delim = getDelimiter();
+  let matched = constants.str.match(constants.delim);
   if (!matched) {
     return [];
   }
-  let numString = removeNewLine(str);
-  let result = numString.split(",");
+  let numString = removeNewLine();
+  let result = numString.split(constants.delim);
   let isValid = true;
   result.forEach((elem) => {
     if (isNaN(parseInt(elem.toString()))) {
+      isValid = false;
+    } else if (parseInt(elem.toString()) < 0) {
       isValid = false;
     }
   });
@@ -35,10 +43,26 @@ function getAllValues(str) {
     return [];
   }
 }
+/**
+ * return an array of all the valid values in input string
+ */
+function getDelimiter(str) {
+  let delim = ",";
+  let delimArr = constants.str.split("//");
+  if (delimArr.length > 1) {
+    delim = delimArr.toString()[1];
+    delimArr[1].split("").pop().toString();
+    constants.str = delimArr[1].substring(1);
+  }
+  return delim;
+}
 
 function removeNewLine(str) {
   let newstr = "";
-  newstr = str.split("\n").join(",");
+  if (constants.str[0] === "\n") {
+    constants.str = constants.str.substring(1);
+  }
+  newstr = constants.str.split("\n").join(constants.delim);
   return newstr;
 }
 module.exports = add;
--- test/sum.js
+++ test/sum.js
@@ -5,46 +5,34 @@ describe("add function ", function () {
   it("with no arguments", function () {
     expect(add()).to.equal(0);
   });
-});
-
-describe("add function ", function () {
   it("with 1 valid number argument only", function () {
     expect(add("1")).to.equal(0);
   });
-});
-
-describe("add function ", function () {
   it("with 1 valid number argument only", function () {
     expect(add("1,")).to.equal(0);
   });
-});
-
-describe("add function ", function () {
   it("with 2 valid number and extra ,", function () {
     expect(add("1,2,")).to.equal(0);
   });
-});
-
-describe("add function ", function () {
   it("with 3 valid number argument only", function () {
     expect(add("1,2,3")).to.equal(6);
   });
-});
-
-describe("add function ", function () {
   it("with 3 valid number argument with \n", function () {
     expect(add("1\n2,3")).to.equal(6);
   });
-});
-
-describe("add function ", function () {
   it("with 2 valid number argument with \n and invalid ,", function () {
     expect(add("1\n2,")).to.equal(0);
   });
-});
-
-describe("add function ", function () {
   it("with only 1 valid number argument with \n", function () {
     expect(add("1\n")).to.equal(0);
+  });
+  it("with only 1 valid number argument with \n and invalid ,", function () {
+    expect(add("1,\n")).to.equal(0);
+  });
+  it("with only 2 valid number argument with \n and custom delimdelimiter ;,", function () {
+    expect(add("//;\n1;2")).to.equal(3);
+  });
+  it("with only 2 valid number argument with \n and custom delimdelimiter ^", function () {
+    expect(add("//^\n1^2\n3^4")).to.equal(10);
   });
 });
